3c18ab2707e20f8c3277b81eda4a657a5bbf2e63
Gymwell19 <ajimuelong@gmail.com>
2025-06-22 15:13:58 +0800

    sword 1

---
+++ constants.py
@@ -0,0 +1,32 @@
+# Achievements
+ACHIEVEMENTS = [
+    "[ACHIEVEMENT] EXECUTIONER: Kill the werewolf by throwing your sword"
+]
+
+# Sword attack flavor text
+SWORD_CRIT = [
+    "[CRITICAL]\n[STUN]\nEither by luck or skill, your blade rips through its hide at the perfect angle.",
+    "[CRITICAL]\n[STUN]\nYou're swift attack brutalizes the creature both physically and emotionally.",
+    "[CRITICAL]\n[STUN]\nYour blade rips through the werewolf with no hesitation. The steel soaks in the blood."
+]
+SWORD_ABOVE_AVG = [
+    "[STUN]\nYour blade slices your enemy and draws blood.",
+    "[STUN]\nYour sword dances in your hand as you slice and dice.",
+    "[STUN]\nYour blade moves so quick its a blur."
+]
+SWORD_AVG = [
+    "Your sword hits your opponent No more, no less.",
+    "You lunge and puncture your opponent. How average.",
+    "Your sword says hello with the werewolf. The werwolf responds by bleeding."
+]
+SWORD_BELOW_AVG = [
+    "Your technique is off and your enthusiasm is pitiful.",
+    "Your blade glances off your opponent like its afraid to hurt them",
+    "Your should have spent more time practicing with your sword rather than polishing it."
+]
+SWORD_MISS = [
+    "Your father raised a disgrace.",
+    "Your blade whistles in the air.",
+    "Nothing happens.",
+    "You're a monke with a stick."
+]
