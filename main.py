da0e175bc2f0a53c44a397cb6d20a952c66855cc
Gymwell19 <ajimuelong@gmail.com>
2025-06-22 17:12:44 +0800

    main

---
+++ main.py
@@ -0,0 +1,18 @@
+from game_logic import Game
+
+def title_screen():
+    # ... (title screen code here)
+    pass
+
+def play_again():
+    # ... (play again logic here)
+    pass
+
+def main():
+    title_screen()
+    game = Game()
+    game.start()
+    play_again()
+
+if __name__ == "__main__":
+    main()
