15a2a4ac679783f5081fc40fa09bbb23f0f1a60e
Gymwell19 <ajimuelong@gmail.com>
2025-06-22 15:23:20 +0800

    wolf moveset

--- constants.py
+++ constants.py
@@ -30,3 +30,35 @@ SWORD_MISS = [
     "Nothing happens.",
     "You're a monke with a stick."
 ]
+
+# Werewolf attack flavor text
+WEREWOLF_CRIT = [
+    "[CRITICAL]\nThe beast is an apex predator, you will not escape.",
+    "[CRITICAL]\nThe werewolf shreds whats left of your torso and will to live.",
+    "[CRITICAL]\nThe creature lunges and sinks its teeth into your arm, if you surive you will suffer for the rest of your life"
+]
+WEREWOLF_ABOVE_AVG = [
+    "The creature strikes a hinge joint and decimates the cartilage beneath.",
+    "The werewolf barrels into you with enough force that it sends you flying to the edge of the clearing.",
+    "The werewolf roars, its reverberating decibal level increases the cortisol production in your body."
+]
+WEREWOLF_AVG = [
+    "The werewolf clubs your body with its meaty paws.",
+    "The creature drop kicks you with its powerful hindquarters.",
+    "The creature slashes your body leaving streaks of dark red behind."
+]
+WEREWOLF_BELOW_AVG = [
+    "The creature's claws glance off your shoulder.",
+    "The werewolf hits you square in the chest, but your adrenaline postpones the pain.",
+    "The beast slashes a part of your body with no blood in it. How peculiar..."
+]
+WEREWOLF_MISS = [
+    "The werewolf scratches your arm enough to make the skin red, but not enough to draw blood. Still hurts...",
+    "The werewolf attempts to clobber you, but instead it hits you with the back of its hand",
+    "The beast swipes at you but instead hits your sword. You manage to hold on but the force crawls up your arm instead."
+]
+WEREWOLF_DEATH = [
+    "The werewolf crumples to the ground and shudders. It yips and growls as it struggles to move.\nAfter a couple minutes the creature slows down until all you can see is its body heave with every breath.",
+    "The beast struggles to stay standing on its hind legs. It whines as it takes one labored step after another.\nThe creature glances at you with something akin to sadness.",
+    "The creature falls on its forelimbs and stares up at the night sky.\nIt howls softly at the moon as if afraid to wake the slumbering forest."
+]
